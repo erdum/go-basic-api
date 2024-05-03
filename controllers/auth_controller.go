ccf8af3f0793ddaf56c7be4fa52920517ec368da
Erdum <erdumadnan@gmail.com>
2024-05-03 15:33:36 +0500

    Feat(Login): Login request validation implemented

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"go-api/services/auth"
+	"go-api/requests"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,18 +12,22 @@ type AuthController struct {
 	authSevice auth.AuthService
 }
 
-type loginPayload struct {
-	IdToken string `json:"idToken"`
-}
-
 func NewAuthController(authSevice auth.AuthService) *AuthController {
 	return &AuthController{authSevice: authSevice}
 }
 
 func (ac *AuthController) Login(context echo.Context) error {
-	payload := loginPayload{}
-	context.Bind(&payload)
+	payload := new(requests.LoginRequest)
+
+	if err := context.Bind(payload); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	if err := context.Validate(payload); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
 
 	data := ac.authSevice.AuthenticateWithThirdParty(payload.IdToken)
+	return context.NoContent(200)
 	return context.JSON(http.StatusOK, data)
 }
