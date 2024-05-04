0183fe310cdd97bf873a8da11f24c80a287ee1cc
Erdum <erdumadnan@gmail.com>
2024-05-04 19:40:09 +0500

    Feat(Logger): Disable file logging when Debug mode is enabled

--- utils/requests_logger.go
+++ utils/requests_logger.go
@@ -2,12 +2,21 @@ package utils
 
 import (
 	"os"
+	"go-api/config"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func SetupHTTPRequestsLogger(app *echo.Echo, logFilePath string) error {
+	appConfig := config.GetConfig()
+	if appConfig.Debug {
+		app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: "method=${method}, uri=${uri}, status=${status} ${latency_human}\n",
+		}))
+		return nil
+	}
+	
 	logFile, err := os.OpenFile(
 		logFilePath,
 		os.O_CREATE|os.O_WRONLY|os.O_APPEND,
